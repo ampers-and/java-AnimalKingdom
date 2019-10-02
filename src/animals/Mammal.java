25915400e34a34deafe0278a99cd91381c7b890d
ampersand <ampersand@ampersands-MacBook-Pro.local>
2019-10-02 15:51:11 -0700

    commit 1

---
+++ src/animals/Mammal.java
@@ -0,0 +1,27 @@
+package animals;
+
+public class Mammal extends Animal
+{
+    public Mammal(String name, int yearDiscovered)
+    {
+        super(name, yearDiscovered);
+    }
+
+    @Override
+    public String move()
+    {
+        return "walk";
+    }
+
+    @Override
+    public String breathe()
+    {
+        return "lungs";
+    }
+
+    @Override
+    public String reproduce()
+    {
+        return "live births";
+    }
+}
